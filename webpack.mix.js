0e3fb911cdc0d3ad18969264d01ceff4c7d3815d
Roman Matviy <roman@matviy.pp.ua>
2023-01-01 18:21:21 +0200

    change public to htdocs

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,18 +11,23 @@ const mix = require("laravel-mix");
  |
  */
 
+// mix.config.publicPath = "public_html";
+
+//! public path
+const public = "public";
+
 //! set path input => output
 const patchInputCSS = "resources/css";
-const patchOutputCSS = "public/css";
+const patchOutputCSS = public + "/css";
 
 const patchInputJS = "resources/js";
-const patchOutputJS = "public/js";
+const patchOutputJS = public + "/js";
 
 // window.$ = window.jQuery = require("jquery");
 
 mix.copy(`${patchInputJS}/**/!(app.js)`, patchOutputJS);
-mix.copy("resources/img/", "public/img");
-mix.copy("resources/svg/", "public/svg");
+mix.copy("resources/img/", public + "/img");
+mix.copy("resources/svg/", public + "/svg");
 
 mix.js(`${patchInputJS}/app.js`, patchOutputJS)
     // .js("resources/js/ie8.js", patchOutputJS)
@@ -43,3 +48,5 @@ mix.js(`${patchInputJS}/app.js`, patchOutputJS)
         require("tailwindcss"),
         require("autoprefixer"),
     ]);
+
+mix.copy("public", "htdocs");
